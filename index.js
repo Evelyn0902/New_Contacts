4a6600d3378f5e5a3541470f55edbaae479068ac
EvelynZhang <2974743840@qq.com>
2024-11-24 12:52:09 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,143 +1,145 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const express = require('express')
+
+const mysql = require('mysql')
+
+const bodyparser = require('body-parser')
+
 const XLSX = require('xlsx');
 
-const app = express();
-const port = 5000;
+const app = express()
 
-// 使用环境变量来管理数据库配置
-const dbConfig = {
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '123456',
-  database: process.env.DB_NAME || 'contacts'
-};
+const connection = mysql.createConnection({
+    host: 'localhost', // 数据库服务器地址
+    user: 'root', // 数据库用户名
+    password: '123456', // 数据库密码
+    database: 'contacts' // 要连接的数据库名
+})
 
-const connection = mysql.createConnection(dbConfig);
+connection.connect()
 
-connection.connect(err => {
-  if (err) {
-    console.error('数据库连接失败: ' + err.stack);
-    return;
-  }
-  console.log('数据库连接成功，ID为: ' + connection.threadId);
-});
+app.use(express.static('pages'))
+app.use(bodyparser.json())
 
-app.use(express.static('pages'));
-app.use(bodyParser.json());
-
-// 路由处理
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/pages/index.html');
-});
+    // 假设你的HTML文件名为index.html，位于public目录下
+    res.sendFile(__dirname + '/pages/index.html')
+})
 
-app.post('/add', async (req, res) => {
-  const { name, phone, email, address } = req.body;
-  if (!name || !phone || !email || !address) {
-    return res.status(400).send('所有字段都是必填项');
-  }
-
-  const query = 'INSERT INTO contacts (name, phone, email, address) VALUES (?, ?, ?, ?)';
-  connection.query(query, [name, phone, email, address], (error, results) => {
-    if (error) {
-      console.error('添加数据时出错:', error.message);
-      return res.status(500).send('内部服务器错误');
-    }
-    res.send('ok');
-  });
-});
+app.post('/add', (req, res) => {
+    let info = req.body
+    console.log(info)
+    const query =
+        'INSERT INTO contacts (name, phone, email, address) VALUES (?, ?, ?, ?)'
+    let values = [info.name, info.phone, info.email, info.address]
+    connection.query(query, values, function(error, results, fields) {
+        if (error) throw error
+            // 成功后的操作，例如输出结果
+        console.log('Row inserted: ', results)
+        res.send('ok')
+    })
+})
 
 app.get('/contacts', (req, res) => {
-  connection.query('SELECT * FROM contacts', (error, results) => {
-    if (error) {
-      console.error('获取数据时出错:', error.message);
-      return res.status(500).send('内部服务器错误');
-    }
-    res.json(results);
-  });
-});
+    connection.query('SELECT * FROM contacts', (error, results, fields) => {
+        if (error) throw error
+            // 发送数据给前端
+        res.json(results)
+    })
+})
 
 app.post('/del', (req, res) => {
-  const { id } = req.body;
-  if (!id) {
-    return res.status(400).send('ID是必填项');
-  }
+    let id = req.body.id
+    const deleteSql = `DELETE FROM contacts WHERE id = ?`
 
-  const deleteSql = 'DELETE FROM contacts WHERE id = ?';
-  connection.query(deleteSql, [id], (error, results) => {
-    if (error) {
-      console.error('删除数据时出错:', error.message);
-      return res.status(500).send('内部服务器错误');
-    }
-    res.send('ok');
-  });
-});
+    // 执行删除操作
+    connection.query(deleteSql, [id], (error, results, fields) => {
+        if (error) {
+            // 处理错误
+            console.error('删除数据时出错:', error.message)
+            return
+        }
+
+        // 删除成功
+        console.log('数据删除成功，影响行数:', results.affectedRows)
+        res.send('ok')
+    })
+})
 
 app.post('/edit', (req, res) => {
-  const { id, name, phone, newphone, email, address } = req.body;
-  if (!id || !name || !email || !address) {
-    return res.status(400).send('所有字段都是必填项');
-  }
-
-  connection.query('SELECT * FROM contacts WHERE id = ?', [id], (error, results) => {
-    if (error) {
-      console.error('更新数据时出错:', error.message);
-      return res.status(500).send('内部服务器错误');
-    }
-
-    let newp = phone;
-    if (newphone) {
-      const currentPhones = results[0].phone ? results[0].phone.split(';') : [];
-      if (!currentPhones.includes(phone)) {
-        newp = `${phone};${results[0].phone}`;
-      }
-    }
-
-    const updateQuery = 'UPDATE contacts SET name = ?, phone = ?, email = ?, address = ? WHERE id = ?';
-    connection.query(updateQuery, [name, newp, email, address, id], (error, results) => {
-      if (error) {
-        console.error('更新数据时出错:', error.message);
-        return res.status(500).send('内部服务器错误');
-      }
-      res.send('ok');
-    });
-  });
-});
+    let info = req.body
+    let newp
+    console.log(info.newphone)
+    connection.query(
+        'SELECT * FROM contacts where id = ' + info.id,
+        (error, results, fields) => {
+            if (error) throw error
+                // 发送数据给前端
+            console.log(results[0].phone)
+            if (results[0].phone == null) {
+                results[0].phone = 'none'
+            }
+            newp = results[0].phone
+            console.log(results[0].phone.split(';'))
+            if (results[0].phone.split(';')[0] != info.phone) {
+                newp = info.phone
+                for (let i = 1; i < results[0].phone.split(';'); i++) {
+                    newp += ';' + results[0].phone.split(';')[i]
+                }
+            }
+            if (info.newphone && results[0].phone.split(';')[0] == info.phone) {
+                newp = results[0].phone + ';' + info.newphone
+            } else if (info.newphone && results[0].phone.split(';')[0] != info.phone) {
+                newp += ';' + info.newphone
+            }
+            const updateQuery =
+                'UPDATE contacts SET name = ?, phone = ?, email = ?, address = ? WHERE id = ?'
+            newData = [info.name, newp, info.email, info.address, info.id]
+            connection.query(updateQuery, newData, function(error, results, fields) {
+                if (error) throw error
+                    // 更新成功的处理逻辑
+                console.log('Row updated successfully')
+                res.send('ok')
+            })
+        }
+    )
+})
 
 app.post('/output', (req, res) => {
-  const data = req.body;
-  const workbook = XLSX.utils.book_new();
-  const xlsdata = [['id', '姓名', '电话', '邮箱', '地址']];
-  data.forEach(item => {
-    xlsdata.push([item.id, item.name, item.phone, item.email, item.address]);
-  });
-  const worksheet = XLSX.utils.aoa_to_sheet(xlsdata);
-  XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-  XLSX.writeFile(workbook, "./pages/output.xlsx");
-  res.send('ok');
-});
+    let data = req.body
+    console.log(data)
+        // 创建工作簿
+    const workbook = XLSX.utils.book_new();
+    let xlsdata = [
+        ['id', '姓名', '电话', '邮箱', '地址']
+    ]
+    data.forEach(item => {
+            xlsdata.push([item.id, item.name, item.phone, item.email, item.address])
+        })
+        // 将数据转换为工作表
+    const worksheet = XLSX.utils.aoa_to_sheet(xlsdata);
+    // 将工作表添加到工作簿
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Sheet1");
+    // 写入文件
+    XLSX.writeFile(workbook, "./pages/output.xlsx");
+    res.send('ok')
+})
 
 app.post('/input', (req, res) => {
-  const data = req.body;
-  data.forEach(item => {
-    const query = 'INSERT INTO contacts (name, phone, email, address) VALUES (?, ?, ?, ?)';
-    connection.query(query, [item['姓名'], item['电话'], item['邮箱'], item['地址']], (error, results) => {
-      if (error) {
-        console.error('导入数据时出错:', error.message);
-        return res.status(500).send('内部服务器错误');
-      }
-    });
-  });
-  res.send('ok');
-});
+    let data = req.body
+    console.log(data)
+    data.forEach(item => {
+        const query =
+            'INSERT INTO contacts (name, phone, email, address) VALUES (?, ?, ?, ?)'
+        let values = [item['姓名'], item['电话'], item['邮箱'], item['地址']]
+        connection.query(query, values, function(error, results, fields) {
+            if (error) throw error
+                // 成功后的操作，例如输出结果
+            console.log('Row inserted: ', results)
+        })
+    })
+    res.send('ok')
+})
 
-// 错误处理中间件
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('内部服务器错误');
-});
-
-app.listen(port, () => {
-  console.log(`服务在http://127.0.0.1:${port}/ 启动`);
-});
+app.listen(5000, () => {
+    console.log('服务在http://127.0.0.1:5000/ 启动')
+})
